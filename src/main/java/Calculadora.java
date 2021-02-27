a676b1aaf96a02dfe46a5cf1f311e79b7b511b6a
Valeria Fornieles <valeriafornieles@gmail.com>
2021-02-27 17:58:22 -0300

    add number input by URL parameters.

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -3,10 +3,10 @@ import io.javalin.Javalin;
 public class Calculadora {
         public static void main(String[] args) {
             Javalin app = Javalin.create().start(7000);
-            app.get("/calculadora/suma", request -> request.result(Integer.toString(suma(2,2))));
-            app.get("/calculadora/resta", request -> request.result(Integer.toString(resta(3,1))));
-            app.get("/calculadora/division", request -> request.result(Float.toString(division(8,2))));
-            app.get("/calculadora/multiplicacion", request -> request.result(Integer.toString(multiplicacion(9,5))));
+            app.get("/calculadora/suma", request -> request.result(Integer.toString(suma(Integer.parseInt(request.queryParam("numero1")),Integer.parseInt(request.queryParam("numero2"))))));
+            app.get("/calculadora/resta", request -> request.result(Integer.toString(resta(Integer.parseInt(request.queryParam("numero1")),Integer.parseInt(request.queryParam("numero2"))))));
+            app.get("/calculadora/division", request -> request.result(Float.toString(division(Integer.parseInt(request.queryParam("numero1")),Integer.parseInt(request.queryParam("numero2"))))));
+            app.get("/calculadora/multiplicacion", request -> request.result(Integer.toString(multiplicacion(Integer.parseInt(request.queryParam("numero1")),Integer.parseInt(request.queryParam("numero2"))))));
         }
 
         private static int suma(int numero1, int numero2) {
